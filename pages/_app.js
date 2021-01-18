93476da21674d6a7c8b9d333c7ace89de5a50953
klim0824 <klim0824@live.jp>
2021-01-18 23:05:31 +0900

    remove unnecessary styles

--- pages/_app.js
+++ pages/_app.js
@@ -5,19 +5,6 @@ import { createGlobalStyle, ThemeProvider } from 'styled-components'
 const GlobalStyle = createGlobalStyle`
   html,
   body {
-    padding: 0;
-    margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-  }
-
-  a {
-    color: inherit;
-    text-decoration: none;
-  }
-
-  * {
-    box-sizing: border-box;
   }
 `
 const theme = {
