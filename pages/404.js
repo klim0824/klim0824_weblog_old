ecae2a1983adb260ce92322dc171ccd820b7376c
klim0824 <klim0824@live.jp>
2021-01-31 02:49:44 +0900

    add Error Page

---
+++ pages/404.js
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+import Layout from '../components/Layout'
+
+export default function Custom404() {
+  return (
+    <Layout place="page">
+      <h1 className="text-center">404 - Page Not Found</h1>
+      <p className="text-center">
+        <Link href="/">
+          <a>ブログトップへ戻る</a>
+        </Link>
+      </p>
+    </Layout>
+  )
+}
