ab33af72f551b5f4a6eec451d40dcba5f08c9ee6
klim0824 <klim0824@live.jp>
2021-02-01 00:02:33 +0900

    minor fix

--- utilities/date.js
+++ utilities/date.js
@@ -3,8 +3,6 @@ import 'dayjs/locale/ja'
 
 const date = (iso8601, delimiter = '/') => {
   dayjs.locale('ja')
-  return (
-    iso8601, dayjs(iso8601).format('YYYY' + delimiter + 'MM' + delimiter + 'DD')
-  )
+  return dayjs(iso8601).format('YYYY' + delimiter + 'MM' + delimiter + 'DD')
 }
 export default date
